fe39847ed58f227ee122b0f5b3d29c26a2cc1004
Horosphere <henrywen99@gmail.com>
2016-09-04 11:42:10 -0700

    Separate function for computing the domain colour

--- DomainColourPlot.m
+++ DomainColourPlot.m
@@ -1,5 +1,6 @@
 BeginPackage["Plotters`"]
 
+DomainColour::usage = "Computes the associated colour with a complex number."
 DomainColourPlot::usage = "Computes domain colouring plot of given complex function."
 
 Options[DomainColourPlot] =
@@ -12,15 +13,16 @@ Options[DomainColourPlot] =
 
 Begin["Private`"]
 
+DomainColour[z_] :=
+	Hue[
+		Arg[z]/(2 \[Pi]),
+		1/(1 + 0.3 Log[Abs[z] + 1]), 
+		1 - 1/(1.1 + 5 Log[Abs[z] + 1])
+	]
 DomainColourPlot[f_, {xMin_, xMax_}, {yMin_, yMax_}, OptionsPattern[]] :=
 	RegionPlot[True,
 		{x, xMin, xMax}, {y, yMin, yMax}, 
-		ColorFunction -> 
-			Function[{x, y}, Hue[
-				Arg[f[x + I y]]/(2 \[Pi]),
-				1/(1 + 0.3 Log[Abs[f[x + I y]] + 1]), 
-				1 - 1/(1.1 + 5 Log[Abs[f[x + I y]] + 1])
-			]], 
+		ColorFunction -> Function[{x, y}, DomainColour[f[x + I y]]],
 		ColorFunctionScaling -> False,
 		PlotPoints -> OptionValue[PlotPoints], 
 		AspectRatio ->
